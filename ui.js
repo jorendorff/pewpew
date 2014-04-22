c2a551eb12dbb2d777dc9eb1f9822926c8dc5189
Jason Orendorff <jason.orendorff@gmail.com>
2014-04-22 15:05:51 -0500

    A few changes to test.html. Add synthesizer (possibly broken).

--- ui.js
+++ ui.js
@@ -1,116 +1,250 @@
+var SynthUI = (function () {
 
-function playAndShowPlot(seed) {
-    var key = String((seed / 100) | 0);
-    var stream = new RC4(key);
-    var rng = new RNG(stream);
-    var params = rngToParams(seed, rng);
-
-    params.sample_size = 16;
-
-    // Force-disable most of the parameters
-    // to see if the waveform looks anything like what's expected.
-
-    // Tone
-    params.p_freq_limit = 0;     // Min frequency cutoff
-    params.p_freq_ramp = 0;      // Slide (SIGNED)
-    params.p_freq_dramp = 0;     // Delta slide (SIGNED)
-    // Vibrato
-    params.p_vib_strength = 0;   // Vibrato depth
-    params.p_vib_speed = 0;      // Vibrato speed
-
-    // Tonal change
-    params.p_arp_mod = 0;        // Change amount (SIGNED)
-    params.p_arp_speed = 0;      // Change speed
-
-    // Duty (wat's that?)
-    params.p_duty = 0;           // Square duty
-    params.p_duty_ramp = 0;      // Duty sweep (SIGNED)
-
-    // Repeat
-    params.p_repeat_speed = 0;   // Repeat speed
-
-    // Phaser
-    params.p_pha_offset = 0;     // Phaser offset (SIGNED)
-    params.p_pha_ramp = 0;       // Phaser sweep (SIGNED)
-
-    // Low-pass filter
-    params.p_lpf_freq = 1.0;     // Low-pass filter cutoff
-    params.p_lpf_ramp = 0;       // Low-pass filter cutoff sweep (SIGNED)
-    params.p_lpf_resonance = 0;  // Low-pass filter resonance
-
-    console.log(uneval(params));
-
-
-    var real_samples = synthesize(params);
-    var wave = samplesToWaveFormat(params.sample_rate, params.sample_size, real_samples);
-    var dataURL = waveFormatToDataURL(wave);
-    playDataURL(dataURL);
-
-    params.p_base_freq /= 5;  // hack params to artificial low frequency
-    var fake_samples = synthesize(params);
-
-    var c = document.getElementById('graph');
-    c.width = real_samples.length;
-    c.height = 400;
-    var ctx = c.getContext("2d");
-
-
-    var labels = [];
-
-
-    function convertToRawData(samples) {
-        var SAMPLE_RATE = 5512;
-
-        var rawData = [];
-        for (var i = 0, o = 0; i <= samples.length; i += 2, o++) {
-            var x = i / SAMPLE_RATE;
-            var yh = samples[i + 1];
-            if (yh >= 0x80)
-                yh -= 0x0100;
-            rawData[o] = (256 * yh + samples[i] + 0.5) / (1 << 15);
-            labels[o] = "";
-
-            if ((o * 10) % SAMPLE_RATE < 10) {
-                var dsec = Math.floor(o * 10 / SAMPLE_RATE);
-                labels[o] = Math.floor(dsec / 10) + "." + (dsec % 10);
-            }
+    // Metadata about all the parameters the synthesizer supports.
+    var param_info = [
+        {
+            id: "env_attack",
+            name: "Attack"
+        },
+        {
+            id: "env_sustain",
+            name: "Sustain"
+        },
+        {
+            id: "env_punch",
+            name: "Punch"
+        },
+        {
+            id: "env_decay",
+            name: "Decay"
+        },
+        {
+            id: "base_freq",
+            name: "Base frequency"
+        },
+        {
+            id: "freq_limit",
+            name: "Frequency limit"
+        },
+        {
+            id: "freq_ramp",
+            name: "Frequency ramp",
+            signed: true
+        },
+        {
+            id: "freq_dramp",
+            name: "Frequency ramp ramp",
+            signed: true
+        },
+        {
+            id: "vib_strength",
+            name: "Vibrato strength"
+        },
+        {
+            id: "vib_speed",
+            name: "Vibrato speed"
+        },
+        {
+            id: "arp_speed",
+            name: "Arpeggio speed"
+        },
+        {
+            id: "arp_mod",
+            name: "Arpeggio magnitude",
+            signed: true
+        },
+        {
+            id: "duty",
+            name: "Square duty"
+        },
+        {
+            id: "duty_ramp",
+            name: "Square duty ramp",
+            signed: true
+        },
+        {
+            id: "repeat_speed",
+            name: "Repeat speed"
+        },
+        {
+            id: "pha_offset",
+            name: "Phaser offset",
+            signed: true
+        },
+        {
+            id: "pha_ramp",
+            name: "Phaser ramp",
+            signed: true
+        },
+        {
+            id: "lpf_freq",
+            name: "Lo-pass frequency"
+        },
+        {
+            id: "lpf_resonance",
+            name: "Lo-pass resonance"
+        },
+        {
+            id: "lpf_ramp",
+            name: "Lo-pass ramp",
+            signed: true
+        },
+        {
+            id: "hpf_freq",
+            name: "Hi-pass frequency"
+        },
+        {
+            id: "hpf_ramp",
+            name: "Hi-pass ramp",
+            signed: true
+        },
+        {
+            id: "sound_vol",
+            name: "Volume"
         }
-        return rawData;
+    ];
+
+    // Slider resolution: jQuery sliders are integer-valued, so we have
+    // to specify how many ticks we want. Here, 1000 for unsigned
+    // parameters, 2000 for signed.
+    var SLIDER_RES = 1000;
+
+    function initControlTable() {
+        var defaults = Params();
+
+        var tbody = $("#controltbody");
+        for (var i = 0; i < param_info.length; i++) {
+            var p = param_info[i];
+            var td = $("<td></td>");
+            td.text(p.name);
+            var slider = $("<td><div class='s' id='" + p.id + "'></div></td>");
+            var row = $("<tr></tr>");
+            row.append(td, slider);
+            tbody.append(row);
+            $("#" + p.id).slider({
+                max: SLIDER_RES,
+                min: p.signed ? -SLIDER_RES : 0,
+                value: defaults[p.id] * SLIDER_RES,
+                slide: onChange,
+                change: onChange
+            });
+        }
     }
 
+    function getParamsFromControls() {
+        var params = new Params;
+        params.wave_type = SQUARE;
+        params.sample_size = 16;
+        params.sample_rate = PUZZLESCRIPT_SAMPLE_RATE;
 
-    var real_samples_raw = convertToRawData(real_samples);
-    var fake_samples_raw = convertToRawData(fake_samples);
+        for (var i = 0; i < param_info.length; i++) {
+            var pi = param_info[i];
+            params[pi.id] = $("#" + pi.id).slider("value") / SLIDER_RES;
+        }
+        return params;
+    }
 
-    var chart = new Chart(ctx);
-    chart.Line({
-        labels: labels,
-        datasets: [
-            {
-                fillColor : "rgba(151,187,205,0.5)",
-                strokeColor : "rgba(151,187,205,1)",
-                data : real_samples_raw
-            },
-            {
-                fillColor: "transparent",
-                strokeColor: "maroon",
-                data: fake_samples_raw
+    function setControls(params) {
+        // Change all the controls to reflect the given params
+        for (var i = 0; i < param_info.length; i++) {
+            var p = param_info[i];
+            onChange.disabled++;
+            $("#" + p.id).slider("value", params[p.id] * SLIDER_RES);
+        }
+    }
+
+    function onChange() {
+        var SOUND_DELAY_MSEC = 400;
+
+        var counter = ++onChange.counter;
+        setTimeout(function () {
+            if (onChange.disabled !== 0) {
+                onChange.disabled--;
+            } else {
+                if (onChange.counter === counter)
+                    playParams(getParamsFromControls());
             }
-        ]
-    }, {
-        bezierCurve: false,
-        pointDot: false,
-        animation: false
-    });
-}
+        }, SOUND_DELAY_MSEC);
+    }
+    onChange.counter = 0;
+    onChange.disabled = 0;
 
-function init_ui() {
-    var seedField = document.getElementById('seed');
-    var playButton = document.getElementById("play");
+    function playSeed(seed) {
+        var key = String((seed / 100) | 0);
+        var stream = new RC4(key);
+        var rng = new RNG(stream);
+        var params = rngToParams(seed, rng);
 
-    playButton.addEventListener("click", function (event) {
-        var seed = parseInt(seedField.value, 10);
-        play(seed);
-        //playAndShowPlot(seed);
-    });
-}
+        return playParams(params);
+    }
+
+    var PUZZLESCRIPT_SAMPLE_RATE = 5512;
+
+    function playParams(params) {
+        setControls(params);
+        params.sample_size = 16;
+        params.sample_rate = PUZZLESCRIPT_SAMPLE_RATE;
+
+        var real_samples = synthesize(params);
+        var wave = samplesToWaveFormat(params.sample_rate, params.sample_size, real_samples);
+        var dataURL = waveFormatToDataURL(wave);
+        playDataURL(dataURL);
+
+        var c = document.getElementById('graph');
+        c.width = real_samples.length;
+        c.height = 400;
+        var ctx = c.getContext("2d");
+
+        var labels = [];
+
+        function convertToRawData(samples) {
+            var SAMPLE_RATE = 5512;
+
+            var rawData = [];
+            for (var i = 0, o = 0; i < samples.length * 0.9; i++, o++) {
+                rawData[o] = samples[i] / (1 << 15);
+                labels[o] = "";
+
+                if ((o * 10) % SAMPLE_RATE < 10) {
+                    var dsec = Math.floor(o * 10 / SAMPLE_RATE);
+                    labels[o] = Math.floor(dsec / 10) + "." + (dsec % 10);
+                }
+            }
+            return rawData;
+        }
+
+
+        var real_samples_raw = convertToRawData(real_samples);
+
+        var chart = new Chart(ctx);
+        chart.Line({
+            labels: labels,
+            datasets: [
+                {
+                    fillColor : "rgba(151,187,205,0.5)",
+                    strokeColor : "rgba(151,187,205,1)",
+                    data : real_samples_raw
+                },
+            ]
+        }, {
+            bezierCurve: false,
+            pointDot: false,
+            animation: false
+        });
+    }
+
+    function init() {
+        var seedField = document.getElementById('seed');
+        var playButton = document.getElementById("play");
+
+        playButton.addEventListener("click", function (event) {
+            var seed = parseInt(seedField.value, 10);
+            playSeed(seed);
+        });
+    }
+
+    return {
+        init: init,
+        initControlTable: initControlTable
+    };
+})();
